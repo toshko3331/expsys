0e5f0b45eaf9779de127362bcdbe3e6215ddadab
DEADMONSTOR <josh14m@aol.com>
2016-01-31 18:37:52 +0000

    Update expsys.lua

--- gamemode/expsys.lua
+++ gamemode/expsys.lua
@@ -31,7 +31,6 @@ function AddExp( ply, exp)
 	sql.Query( "UPDATE experience SET EXP = EXP + '"..exp.."' WHERE SteamID = '"..steamID.."'" )
 	UpdateClientExp(ply,sql.QueryValue("SELECT EXP FROM experience WHERE SteamID = '"..steamID.."'"))
        	ply:SendLua("notification.AddLegacy('You got XP!', NOTIFY_GENERIC, 5);")
-	Level(ply, exp)
 end
 
 function SetExp(ply , exp)
@@ -39,7 +38,6 @@ function SetExp(ply , exp)
 	sql.Query( "UPDATE experience SET EXP = '"..exp.."' WHERE SteamID = '"..steamID.."'" )
 	UpdateClientExp(ply,exp)
        	ply:SendLua("notification.AddLegacy('You got XP!', NOTIFY_GENERIC, 5);")
-	Level(ply, exp)
 end
 
 function UpdateClientExp(ply, exp)
@@ -47,6 +45,8 @@ function UpdateClientExp(ply, exp)
 	net.Start( "UpdateExp" )
 	net.WriteInt(exp,32)
 	net.Send(ply)
+	exp2 = tonumber(exp,10)
+	Level(ply, exp2)
 end
 
 function UpdateClientLevel(ply, Level)
@@ -58,23 +58,24 @@ end
 
 Levels = {1,10,50,70,80,300,600,900,100000}
 ActualLevels = {1,2,3,4,5,6,7,8,9,10}
-function Level(ply, exp)
+function Level(ply, exp2)
 	local steamID = ply:SteamID()
 	for k,v in pairs(player.GetAll()) do
-		for x,z in pairs(Levels) do
-			if Levels[x] == 1 then
-				if exp < Levels[x] then
-					ply:ChatPrint(ActualLevels[x])
-					UpdateClientLevel(ply, ActualLevels[x])
-					print("X:"..ActualLevels[x])
+		for p,z in pairs(Levels) do
+			if Levels[p] == 1 then
+				if exp2 < Levels[p] then
+					ply:ChatPrint(ActualLevels[p])
+					UpdateClientLevel(ply, ActualLevels[p])
+					print("p:"..ActualLevels[p])
 				end
 			end
-			if Levels[x] != 1 then
-				if exp > Levels[x-1] then
-					if exp <= Levels[x] then   
-						ply:ChatPrint(ActualLevels[x])
-						UpdateClientLevel(ply, ActualLevels[x])
-						print("X:"..ActualLevels[x])
+			if Levels[p] != 1 then
+				if exp2 > Levels[p-1] then
+				print("Done")
+					if exp2 <= Levels[p] then   
+						ply:ChatPrint(ActualLevels[p])
+						UpdateClientLevel(ply, ActualLevels[p])
+						print("p:"..ActualLevels[p])
 					end
 				end
 			end
