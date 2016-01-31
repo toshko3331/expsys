5d73a73ae5d9b2246099ed82c4e968ad715d9546
DEADMONSTOR <josh14m@aol.com>
2016-01-31 20:32:57 +0000

    Update cl_expsys.lua

--- gamemode/cl_expsys.lua
+++ gamemode/cl_expsys.lua
@@ -9,6 +9,7 @@ net.Receive("UpdateExp",UpdateExp)
 function UpdateLevel(len)
 	Level = net.ReadInt(32)
 	print("I have been updated! Level: "..Level)
+	test(Level)
 end
 net.Receive("UpdateLevel",UpdateLevel)
 
@@ -16,3 +17,30 @@ function PrintExp()
 	print(experience)
 end
 concommand.Add("print_exp",PrintExp)
+
+hook.Add( "HUDPaint", "HelloThere", function()
+	draw.DrawText( "Level: "..Level, "DermaLarge", ScrW() * 0.07 , ScrH() * 0.85, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER )
+end )
+
+function test()
+	local ply = LocalPlayer()
+	
+	for id,target in pairs(ents.FindByClass("Player")) do
+		local name = tostring(target:Nick())
+		
+		local steamid = tostring(target:SteamID())
+		local targetPos = target:GetPos() + Vector(0,0,35)
+		
+		local targetDistance = math.floor((target:GetPos():Distance( targetPos ))/40)
+		
+		local targetScreenpos = targetPos:ToScreen()
+		if targetDistance > 20 then end
+		if targetDistance < 20 then
+			surface.SetTextColor(200,25,25,255)
+			surface.SetFont( "Default" )
+			surface.SetTextPos( tonumber(targetScreenpos.x), tonumber(targetScreenpos.y))
+			surface.DrawText("Player Level: ".. target:GetVar("Level", 0 ))
+	end
+	end
+end
+hook.Add( "HUDPaint", "DrawStuff", test)
