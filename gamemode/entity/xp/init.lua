1fce4f0c036b604f1e588e5180751ed22b8a7553
DEADMONSTOR <josh14m@aol.com>
2016-02-02 11:50:48 +0000

    Update init.lua

--- gamemode/entity/xp/init.lua
+++ gamemode/entity/xp/init.lua
@@ -17,9 +17,9 @@ function ENT:Initialize()
 end
  
 function ENT:Use( activator, caller )
-    AddExp(activator, 20)
+   EPSYS.AddXP(activator,20)
 end
  
 function ENT:Think()
-    -- We don't need to think, we are just a prop after all!
+	----
 end
